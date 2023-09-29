bca2816a81dd940539e285105cf80112f0a22b94
ltbreno <132773962+ltbreno@users.noreply.github.com>
2023-09-29 12:31:46 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CardComponent } from './components/card/card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { CardLabelComponent } from './components/card/card-label/card-label.component';
 import { CardPriceComponent } from './components/card/card-price/card-price.component';
+import { AnimatedComponent } from './components/animated/animated.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CardPriceComponent } from './components/card/card-price/card-price.comp
     CardComponent,
     MenuBarComponent,
     CardLabelComponent,
-    CardPriceComponent
+    CardPriceComponent,
+    AnimatedComponent
   ],
   imports: [
     BrowserModule,
