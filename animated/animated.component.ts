3ade5ca5dc6b4a67faab2f065af8bc64f6d745de
ltbreno <132773962+ltbreno@users.noreply.github.com>
2023-09-29 12:30:51 -0300

    Add files via upload

---
+++ animated/animated.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animated',
+  templateUrl: './animated.component.html',
+  styleUrls: ['./animated.component.css']
+})
+export class AnimatedComponent {
+
+}
